608730befc50e65a459b695be0a18d75e4fc1c83
yahyaSurvive <putuyahya5566@gmail.com>
2022-09-25 13:36:57 +0800

    ganti warna aja bang

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <Navbar bg="success" variant="dark">
+      <Navbar bg="danger" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
